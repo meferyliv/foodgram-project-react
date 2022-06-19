c6b1a9fd241ee5e0d6929f2cf6502219dde2d027
mefery <mefery@gmail.com>
2022-06-19 19:29:00 +0300

    Second review

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_KEY')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='backend localhost').split()
 
