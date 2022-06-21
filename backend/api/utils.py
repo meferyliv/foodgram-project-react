79a58dd456948374ca26c11af0770e48aefddfb4
mefery <mefery@gmail.com>
2022-06-21 19:43:28 +0300

    Third review

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,22 +1,43 @@
-from django.db.models import Sum
 from django.http import HttpResponse
-from django.template.loader import render_to_string
-from weasyprint import HTML
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen.canvas import Canvas
 
 from recipes.models import IngredientAmount
 
 
 def generate_shopping_list(request):
     ingredients = IngredientAmount.objects.filter(
-        recipe__carts__user=request.user.id
-    ).annotate(amount=Sum('amount')).values_list(
-        'ingredient__name', 'amount', 'ingredient__measurement_unit'
+        recipe__carts__user=request.user.id).values_list(
+        'ingredient__name', 'ingredient__measurement_unit', 'amount'
     )
-    html_template = render_to_string(
-        'recipes/template_shop_list.html', {'ingredients': ingredients}
+    ingredients_dict = {}
+    for item in ingredients:
+        name = item[0]
+        if name not in ingredients_dict:
+            ingredients_dict[name] = {
+                'measurement_unit': item[1],
+                'amount': item[2]
+            }
+        else:
+            ingredients_dict[name]['amount'] += item[2]
+    pdfmetrics.registerFont(
+        TTFont('Slimamif', 'Slimamif.ttf', 'UTF-8')
     )
-    html = HTML(string=html_template)
-    result = html.write_pdf()
-    response = HttpResponse(result, content_type='application/pdf;')
-    response['Content-Disposition'] = 'inline; filename=shopping_list.pdf'
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = (
+        'attachment; filename="shopping_list.pdf"'
+    )
+    page = Canvas(response)
+    page.setFont('Slimamif', size=24)
+    page.drawString(200, 800, 'Список ингредиентов')
+    page.setFont('Slimamif', size=16)
+    height = 750
+    for i, (name, data) in enumerate(ingredients_dict.items(), 1):
+        page.drawString(75, height, (
+            f'<{i}> {name} - {data["amount"]}, {data["measurement_unit"]}'
+        ))
+        height -= 25
+    page.showPage()
+    page.save()
     return response
